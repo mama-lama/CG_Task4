f68f78b0ab2278c11081cbac3c1861a731b91f0a
MoonBunny70 <dsidelnikov671@gmail.com>
2025-12-15 00:37:59 +0300

    Basic methods and constructors have been added to the Vector3f class.

--- src/main/java/com/cgvsu/math/Vector3f.java
+++ src/main/java/com/cgvsu/math/Vector3f.java
@@ -2,17 +2,42 @@ package com.cgvsu.math;
 
 // Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector3f {
+    private float x;
+    private float y;
+    private float z;
+
+    private final float EPS = 1e-7f;
+
+    // ---------------------- КОНСТРУКТОРЫ ----------------------------
+    public Vector3f() {
+        this(0, 0, 0);
+    }
     public Vector3f(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
-        final float eps = 1e-7f;
-        return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
+    // ----------------------- ГЕТЕРЫ ---------------------------------
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+    public float getZ() {
+        return z;
     }
 
-    public float x, y, z;
+    // ----------------------- СЕТТЕРЫ -----------------------------------
+    public void setX(float x) {
+        this.x = x;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setZ(float z) {
+        this.z = z;
+    }
+
 }
