f2210044b24b74d40f9c04ce07abd9cfe66599ec
MoonBunny70 <dsidelnikov671@gmail.com>
2025-12-15 02:46:30 +0300

    Methods for working with matrices have been added to the Matrix3 class.

--- src/main/java/com/cgvsu/math/matrices/Matrix3.java
+++ src/main/java/com/cgvsu/math/matrices/Matrix3.java
@@ -1,5 +1,7 @@
 package com.cgvsu.math.matrices;
 
+import com.cgvsu.math.vectors.Vector3f;
+
 public class Matrix3 {
     private float[][] data = new float[3][3];
 
@@ -35,6 +37,69 @@ public class Matrix3 {
     // Нулевая
     public static Matrix3 zeroMatrix() {
         return new Matrix3();
+    }
+
+    // --------------------------- РАБОТА С МАТРИЦАМИ --------------------------
+    // Сложение
+    public Matrix3 addition(Matrix3 other) {
+        float[][] result = new float[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = this.data[i][j] + other.data[i][j];
+            }
+        }
+
+        return new Matrix3(result);
+    }
+    // Вычитание
+    public Matrix3 subtraction(Matrix3 other) {
+        float[][] result = new float[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = this.data[i][j] - other.data[i][j];
+            }
+        }
+
+        return new Matrix3(result);
+    }
+    // Умножение на вектор
+    public Vector3f multiplication(Vector3f vector) {
+        float x = data[0][0] * vector.getX() + data[0][1] * vector.getY() + data[0][2] * vector.getZ();
+        float y = data[1][0] * vector.getX() + data[1][1] * vector.getY() + data[1][2] * vector.getZ();
+        float z = data[2][0] * vector.getX() + data[2][1] * vector.getY() + data[2][2] * vector.getZ();
+
+        return new Vector3f(x, y, z);
+    }
+    // Умножение на матрицу
+    public Matrix3 multiplication(Matrix3 other) {
+        float[][] result = new float[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                float sum = 0;
+                for (int k = 0; k < 3; k++) {
+                    sum += this.data[i][k] * other.data[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+
+        return new Matrix3(result);
+    }
+
+    // Транспонирование
+    public Matrix3 transposition() {
+        float[][] result = new float[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[j][i] = this.data[i][j];
+            }
+        }
+
+        return new Matrix3(result);
     }
 
     // ------------------------- ВЫВОД МАТРИЦЫ -----------------------------------
