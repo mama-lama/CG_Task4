422447244f17e69df416b46eacf31c10d029abd9
MoonBunny70 <dsidelnikov671@gmail.com>
2025-12-15 02:38:09 +0300

    The ability to create identity and zero matrices has been added to the Matrix3 class.

--- src/main/java/com/cgvsu/math/matrices/Matrix3.java
+++ src/main/java/com/cgvsu/math/matrices/Matrix3.java
@@ -1,11 +1,11 @@
 package com.cgvsu.math.matrices;
 
 public class Matrix3 {
-    private double[][] data = new double[3][3];
+    private float[][] data = new float[3][3];
 
     // -------------------------- Конструкторы ---------------------------
     public Matrix3() {}
-    public Matrix3(double[][] data) {
+    public Matrix3(float[][] data) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 this.data[i][j] = data[i][j];
@@ -17,8 +17,24 @@ public class Matrix3 {
     public double get(int row, int col) {
         return data[row][col];
     }
-    public void set(int row, int col, double value) {
+    public void set(int row, int col, float value) {
         data[row][col] = value;
+    }
+
+    // ----------------------- СОЗДАНИЕ МАТРИЦ ---------------------
+    // Единичная
+    public static Matrix3 identityMatrix() {
+        float[][] id = new float[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            id[i][i] = 1.0F;
+        }
+
+        return new Matrix3(id);
+    }
+    // Нулевая
+    public static Matrix3 zeroMatrix() {
+        return new Matrix3();
     }
 
     // ------------------------- ВЫВОД МАТРИЦЫ -----------------------------------
