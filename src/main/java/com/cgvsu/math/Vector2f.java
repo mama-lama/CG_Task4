6ef5b5e32b25216e35f39671ad807ddf19f48eff
MoonBunny70 <dsidelnikov671@gmail.com>
2025-12-14 23:12:28 +0300

    Added .gitignore. Basic methods and constructors have been added to the Vector2f class.

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -1,11 +1,36 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector2f {
+    private float x;
+    private float y;
+
+    // --------------- КОНСТРУКТОРЫ --------------------
+    public Vector2f() {
+        this(0,0);
+    }
     public Vector2f(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    public float x, y;
+    // ---------------- ГЕТЕРЫ ---------------------------
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+
+    // ----------------- СЕТТЕРЫ --------------------------
+    public void setX(float newX) {
+        this.x = newX;
+    }
+    public void setY(float newY) {
+        this.y = newY;
+    }
+
+    // ---------------------- ВЫВОД ВЕКТОРА -------------------
+    public String toString() {
+        return "(" + x + "; " + y + ")";
+    }
 }
